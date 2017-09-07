f6213dfd3d3702bec325a7730589a8e5890fe2e6
calebeich7 <dalethreesixty@outlook.com>
2017-09-06 19:00:20 -0700

    donezo

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,7 @@ import Stopwatch from './stopwatch';
 export default (props) => {
     return (
         <div className="container">
-            <h1 className="text-center"> Stopwatch</h1>
+            <h1 className="text-center"> React Stopwatch</h1>
             <Stopwatch/>
         </div>
     )
